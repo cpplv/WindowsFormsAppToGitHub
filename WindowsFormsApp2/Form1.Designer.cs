ea67d2197f852acbb09ffa0559f85540629a0e6a
agaspars <andrejs.gaspars@gmail.com>
2022-01-26 13:43:29 +0200

    Gaspars

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.buttonAndrejs = new System.Windows.Forms.Button();
+            this.buttonGaspars = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -50,11 +51,22 @@
             this.buttonAndrejs.UseVisualStyleBackColor = true;
             this.buttonAndrejs.Click += new System.EventHandler(this.button1_Click);
             // 
+            // buttonGaspars
+            // 
+            this.buttonGaspars.Location = new System.Drawing.Point(98, 53);
+            this.buttonGaspars.Name = "buttonGaspars";
+            this.buttonGaspars.Size = new System.Drawing.Size(75, 23);
+            this.buttonGaspars.TabIndex = 2;
+            this.buttonGaspars.Text = "Gaspars";
+            this.buttonGaspars.UseVisualStyleBackColor = true;
+            this.buttonGaspars.Click += new System.EventHandler(this.buttonGaspars_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(502, 290);
+            this.Controls.Add(this.buttonGaspars);
             this.Controls.Add(this.buttonAndrejs);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonAndrejs;
+        private System.Windows.Forms.Button buttonGaspars;
     }
 }
 
